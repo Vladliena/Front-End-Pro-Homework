7b2b3e103568f12377dd67cefd35ed761b3932e9
vladliena <sotnikovavladlena@gmail.com>
2022-11-30 18:05:55 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+//
+// let firstNumber = 0;
+// let secondNumber = 0;
+//
+// while (!firstNumber || isNaN(firstNumber)){
+//     firstNumber = prompt('add first number')
+//     if (firstNumber){
+//         secondNumber = prompt('add second number')
+//         if (!secondNumber || isNaN(secondNumber)){
+//             secondNumber = prompt('add second number')
+//         }
+//     }
+// }
+//
+// let result = firstNumber+secondNumber
+//
+// alert(result)
+
+// let firstNumber = parseFloat(prompt('add first number'));
+// let secondNumber = parseFloat(prompt('add second number'));
+// let plusSum = firstNumber + secondNumber;
+// let minusSum = firstNumber - secondNumber;
+// let multiplicationSum = firstNumber * secondNumber;
+// let divisionSum = firstNumber / secondNumber;
+//
+// alert(`${firstNumber} + ${secondNumber} = ${plusSum}
+// ${firstNumber} - ${secondNumber} = ${minusSum}
+// ${firstNumber} * ${secondNumber} = ${multiplicationSum}
+// ${firstNumber} / ${secondNumber} = ${divisionSum}`)
+
+let firstNumber
