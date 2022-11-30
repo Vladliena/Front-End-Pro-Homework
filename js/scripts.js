50d56ce4df83570c0cb4d5c1ce9f57cd1f221d38
vladliena <sotnikovavladlena@gmail.com>
2022-11-30 18:19:57 +0200

    Finished homework

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,20 @@
-//
-// let firstNumber = 0;
-// let secondNumber = 0;
-//
-// while (!firstNumber || isNaN(firstNumber)){
-//     firstNumber = prompt('add first number')
-//     if (firstNumber){
-//         secondNumber = prompt('add second number')
-//         if (!secondNumber || isNaN(secondNumber)){
-//             secondNumber = prompt('add second number')
-//         }
-//     }
-// }
-//
-// let result = firstNumber+secondNumber
-//
-// alert(result)
+let firstNumber;
+let secondNumber;
 
-// let firstNumber = parseFloat(prompt('add first number'));
-// let secondNumber = parseFloat(prompt('add second number'));
-// let plusSum = firstNumber + secondNumber;
-// let minusSum = firstNumber - secondNumber;
-// let multiplicationSum = firstNumber * secondNumber;
-// let divisionSum = firstNumber / secondNumber;
-//
-// alert(`${firstNumber} + ${secondNumber} = ${plusSum}
-// ${firstNumber} - ${secondNumber} = ${minusSum}
-// ${firstNumber} * ${secondNumber} = ${multiplicationSum}
-// ${firstNumber} / ${secondNumber} = ${divisionSum}`)
+do {
+    firstNumber = parseFloat(prompt('add first number'));
+} while (!firstNumber || isNaN(firstNumber))
 
-let firstNumber
+do {
+    secondNumber = parseFloat(prompt('add second number'));
+} while (!secondNumber || isNaN(secondNumber))
+
+const plusSum = firstNumber + secondNumber;
+const minusSum = firstNumber - secondNumber;
+const multiplicationSum = firstNumber * secondNumber;
+const divisionSum = firstNumber / secondNumber;
+
+alert(`${firstNumber} + ${secondNumber} = ${plusSum}
+${firstNumber} - ${secondNumber} = ${minusSum}
+${firstNumber} * ${secondNumber} = ${multiplicationSum}
+${firstNumber} / ${secondNumber} = ${divisionSum}`)
